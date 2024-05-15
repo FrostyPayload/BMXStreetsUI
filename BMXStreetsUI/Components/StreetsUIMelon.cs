205e454a7a77459425c4c5590c0a674ef2e5e1c4
FrostyPayload <77692236+FrostyPayload@users.noreply.github.com>
2024-05-16 00:53:31 +0100

    version check

--- BMXStreetsUI/Components/StreetsUIMelon.cs
+++ BMXStreetsUI/Components/StreetsUIMelon.cs
@@ -1,5 +1,7 @@
 ﻿using BmxStreetsUI.Components;
+using Il2CppTriangleNet;
 using MelonLoader;
+using UnityEngine;
 
 [assembly: MelonInfo(typeof(StreetsUIMelon), "BmxStreetsUI", "1.0.0", "FrostyP/LineRyder")]
 [assembly: MelonGame()]
@@ -11,7 +13,20 @@ namespace BmxStreetsUI.Components
         public override void OnInitializeMelon()
         {
             Log.logger = LoggerInstance;
+        }
+        
+        bool VersionNotAcceptable()
+        {
+            string minVer = "1.0.0.136.0";
+            string versionString = Application.version;
+            // Find the first occurrence of "[" and "]"
+            int startIndex = versionString.IndexOf('[');
+            int endIndex = versionString.IndexOf(']');
 
+            // Extract the substring between "[" and "]"
+            string versionNumber = versionString.Substring(startIndex + 1, endIndex - startIndex - 1);
+
+            return versionNumber.CompareTo(minVer) < 0;
         }
         public override void OnSceneWasInitialized(int buildIndex, string sceneName)
         {
@@ -19,6 +34,12 @@ namespace BmxStreetsUI.Components
             {
                 if (sceneName.ToLower().Contains(Constants.MainMenuSceneName))
                 {
+                    if (VersionNotAcceptable())
+                    {
+                        LoggerInstance.Error($"BMXStreetsUI shut down due to internal error");
+                        return;
+                    }
+
                     StreetsUI.Initialize();
                 }
             }
